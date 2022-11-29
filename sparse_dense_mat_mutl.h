1321ed057eed418b2749ea664ff942313372454e
JT <jtvinolus@gmail.com>
2022-11-29 11:22:33 -0800

    init

---
+++ sparse_dense_mat_mutl.h
@@ -0,0 +1,28 @@
+#ifndef SPARSE_DENSE_MAT_MULT
+#define SPARSE_DENSE_MAT_MULT
+
+#include <cassert>
+#include <stdio.h>
+#include <stdlib.h>
+#include <cstring>
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+
+
+#include "sparse_matrix.h"
+#include "utility_functions.h"
+
+#ifdef OPEN_MP_PROJECT
+#include "omp.h"
+
+#elif OPEN_ACC_PROJECT
+
+#else
+// single threaded
+#endif
+
+
+void SparseDenseMatMult(SparseMatrix *adjM, flt32 *initPgRnkV, flt32 *finPgRnkV, uns32 N);
+#endif
